041446ca3064afe5bb2b1a0fef211b625f038afe
moonsoo.kwon <moonsoo.kwon@seeroo.co.kr>
2025-06-27 12:42:43 +0900

    feat) 이터레이터 패턴, 카페메뉴 실습

---
+++ src/main/java/org/example/e09_iterator/dinermergercafe/Menu.java
@@ -0,0 +1,7 @@
+package org.example.e09_iterator.dinermergercafe;
+
+import java.util.Iterator;
+
+public interface Menu {
+	public Iterator<MenuItem> createIterator();
+}
---
+++ src/main/java/org/example/e09_iterator/dinermergeri/Menu.java
@@ -0,0 +1,7 @@
+package org.example.e09_iterator.dinermergeri;
+
+import java.util.Iterator;
+
+public interface Menu {
+	public Iterator<MenuItem> createIterator();
+}
